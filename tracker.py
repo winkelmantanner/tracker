d6da286b337775b620805b178f79f0b0aa35fedb
Lucas <lawf48@mst.edu>
2018-11-07 14:51:38 -0600

    Made some more progress.

--- tracker.py
+++ tracker.py
@@ -4,8 +4,6 @@
 import os
 import sys
 from socket import *
-for root, subdirs, files in os.walk(os.getcwd()):
-    print ( root , subdirs , files )
 
 
 def CreateRepository ( Folder ) :
@@ -21,8 +19,9 @@ def HostRepositories ( ) :
     print("Waiting to receive messages...")
     while True:
         (data, client_address) = UDPSock.recvfrom(buf)
-        print("Received message: " + data)
-        DataArray = data . split()
+        Message = data.decode()
+        print("Received message: " + Message)
+        DataArray = Message . split ( )
         print(DataArray)
         if data == "exit":
             break
@@ -34,16 +33,18 @@ def HostRepositories ( ) :
     os._exit(0)
 
 def SendRepository ( RootFolder , client_address ) :
+    print('Sending repository \'' + RootFolder + '\' to ' + str ( client_address ) )
     Buffer = ''
     for root, subdirs, files in os.walk(os.getcwd() + '/' + RootFolder):
         Buffer = Buffer + '[' + root + '] folder\n'
+        print ( Buffer )
         for SubFile in files :
             FilePath = root + '/' + SubFile
             Size = os.path.getsize(FilePath)
-            with open(SubFile, 'r') as content_file:
+            with open(FilePath, 'r') as content_file:
                 content = content_file.read()
             Buffer = Buffer + '[' + root + '] file ' + Size + ' ' + Contents + '\n'
-    print ( Buffer )
+            print ( Buffer )
     
 
 def RetrieveRemoteRepository ( Folder , RemoteAddress ) :
