e61afaf8464e5d127bea0847f79a68ee4bef6def
Tanner Winkelman <winkelmantanner@gmail.com>
2018-11-20 19:37:08 -0600

    Detect file changes.

--- file_tree_loader.py
+++ file_tree_loader.py
@@ -1,10 +1,13 @@
 import os
+import sys
 import base64
 
-def load_file_contents_base64(path_string):
+from diff import diff_trees
+
+def load_file_contents(path_string):
     with open(path_string, 'rb') as bytes_generator:
         file_bytes = bytes_generator.read()
-    return base64.b64encode(file_bytes)
+    return file_bytes
 
 def load_one_file(tree, path_string_to_containing_folder, file_name):
     path_list = path_string_to_containing_folder.split(os.sep)
@@ -18,15 +21,20 @@ def load_one_file(tree, path_string_to_containing_folder, file_name):
         else:
             current_position[directory] = {}
             current_position = current_position[directory]
-    current_position[file_name] = load_file_contents_base64(path_string_to_containing_folder + str(os.sep) + file_name)
+    current_position[file_name] = load_file_contents(path_string_to_containing_folder + str(os.sep) + file_name)
 
 
 def load_tree(root):
-    tree = dict()
-    for path_string, dir_list, file_list in os.walk(root):
-        for file_name in file_list:
-            load_one_file(tree, path_string, file_name)
-    print(tree)
+    tree_a = dict()
+    for line in sys.stdin:
+        tree_b = dict()
+        for path_string, dir_list, file_list in os.walk(root):
+            for file_name in file_list:
+                load_one_file(tree_b, path_string, file_name)
+        left_diff_tree, right_diff_tree = diff_trees(tree_a, tree_b)
+        print(left_diff_tree, right_diff_tree)
+        tree_a = tree_b
+
 
 if __name__=='__main__':
     load_tree('.')
