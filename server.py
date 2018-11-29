57a2ad619a84e2be18ca5648d1d4da0395f1c942
Tanner Winkelman <winkelmantanner@gmail.com>
2018-11-29 16:34:43 -0600

    Remove django

--- server.py
+++ server.py
@@ -1,13 +1,3 @@
-import sys
-import pickle
-
-import file_tree_loader
-
-from django.conf import settings
-from django.conf.urls import url
-from django.core.management import execute_from_command_line
-from django.views import View
-from django.http import HttpResponse
 
 LOCAL_REPO_KEY = 'local_name'
 REMOTE_REPO_KEY = 'remote_name'
@@ -16,25 +6,3 @@ FILE_DICT_KEY = 'file_dict'
 OPERATION_KEY = 'operation'
 UPLOAD = 'upload'
 DOWNLOAD = 'download'
-
-
-class Index(View):
-    def get(self, request):
-        return HttpResponse('<h1>A minimal Django response!</h1>')
-    def post(self, request):
-        data = pickle.loads(request.body)
-        state_name = data[STATE_NAME_KEY]
-        file_dict = data[FILE_DICT_KEY]
-        save_result = file_tree_loader.save_dict(state_name, file_dict)
-        return HttpResponse(save_result)
-
-urlpatterns = [
-    url(r'^$', Index.as_view()),
-]
-
-if __name__ == '__main__':
-    settings.configure(
-        DEBUG=False,
-        ROOT_URLCONF=sys.modules[__name__],
-    )
-    execute_from_command_line(sys.argv + ['runserver'])
