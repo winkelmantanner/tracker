72d295e98139ea958b8b4e88aaea2271fea8acfa
Tanner Winkelman <tjwy9b@mst.edu>
2018-11-29 12:37:58 -0600

    Revert "Upload appears to work.  Try `python server.py`"
    This reverts commit b4a5149383d27a700032859f2ac10f3aca769621.

--- server.py
+++ server.py
@@ -1,35 +1,22 @@
 import sys
-import pickle
-
-import file_tree_loader
 
 from django.conf import settings
 from django.conf.urls import url
 from django.core.management import execute_from_command_line
-from django.views import View
 from django.http import HttpResponse
 
-STATE_NAME_KEY = 'state_name'
-FILE_DICT_KEY = 'file_dict'
+settings.configure(
+    DEBUG=True,
+    ROOT_URLCONF=sys.modules[__name__],
+)
 
 
-class Index(View):
-    def get(self, request):
-        return HttpResponse('<h1>A minimal Django response!</h1>')
-    def post(self, request):
-        data = pickle.loads(request.body)
-        state_name = data[STATE_NAME_KEY]
-        file_dict = data[FILE_DICT_KEY]
-        save_result = file_tree_loader.save_dict(state_name, file_dict)
-        return HttpResponse(save_result)
+def index(request):
+    return HttpResponse('<h1>A minimal Django response!</h1>')
 
 urlpatterns = [
-    url(r'^$', Index.as_view()),
+    url(r'^$', index),
 ]
 
 if __name__ == '__main__':
-    settings.configure(
-        DEBUG=True,
-        ROOT_URLCONF=sys.modules[__name__],
-    )
     execute_from_command_line(sys.argv + ['runserver'])
